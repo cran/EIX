ddb5d7351dda06faabed256970e50466112a5369
Ewelina Karbowiak <ewelina.karbowiak12@gmail.com>
2020-02-18 21:40:06 +0000

    version 1.1

--- inst/doc/EIX.R
+++ inst/doc/EIX.R
@@ -1,28 +1,28 @@
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 knitr::opts_chunk$set(fig.height = 6, fig.width = 6, fig.align = "center")
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #devtools :: install_github("ModelOriented/EIX")
 library("EIX")
 set.seed(4)
 knitr::kable(head(HR_data))
 
-## ---- warning=FALSE, message=FALSE---------------------------------------
+## ---- warning=FALSE, message=FALSE--------------------------------------------
 library("Matrix")
 sparse_matrix <- sparse.model.matrix(left ~ . - 1,  data = HR_data)
 head(sparse_matrix)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library("xgboost")
 param <- list(objective = "binary:logistic", max_depth = 2)
 xgb_model <- xgboost(sparse_matrix, params = param, label = HR_data[, left] == 1, nrounds = 50, verbose = FALSE)
 knitr::kable(head(xgboost::xgb.model.dt.tree(colnames(sparse_matrix),xgb_model)))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 knitr::kable(head(xgboost::xgb.importance(colnames(sparse_matrix),xgb_model)))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lolli<-lollipop(xgb_model,sparse_matrix)
 plot(lolli)
 #plot(lolli, threshold=0.05)
@@ -32,29 +32,29 @@ plot(lolli)
 #plot(lolli, labels="interactions",threshold=0.05)
 #plot(lolli, log_scale = FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 pairs<-interactions(xgb_model, sparse_matrix, option = "pairs")
 head(pairs)
 plot(pairs)
 
-## ---- warning=FALSE, message=FALSE---------------------------------------
+## ---- warning=FALSE, message=FALSE--------------------------------------------
 interactions<-interactions(xgb_model, sparse_matrix, option = "interactions")
 head(interactions)
 plot(interactions)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 importance<-importance(xgb_model, sparse_matrix, option = "both")
 head(importance)
 plot(importance, radar=FALSE)
 #plot(importance,  xmeasure = "mean5Gain", ymeasure = "sumGain",  top = 15, radar=FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(importance)
 #plot(importance, text_start_point = 0.3)
 #plot(importance, text_size = 4)
 #plot(importance, top=15)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 data <- HR_data[9,]
 new_observation <- sparse_matrix[9,]
 wf<-waterfall(xgb_model, new_observation, data, option = "interactions")
--- inst/doc/titanic_data.R
+++ inst/doc/titanic_data.R
@@ -1,8 +1,8 @@
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 knitr::opts_chunk$set(fig.height = 6, fig.width = 6, fig.align = "center")
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #devtools :: install_github("ModelOriented/EIX")
 library("EIX")
 library(data.table)
@@ -13,29 +13,29 @@ knitr::kable(head(titanic_data))
 library("Matrix")
 sparse_matrix <- sparse.model.matrix(survived ~ . - 1,  data = titanic_data)
 
-## ---- warning=FALSE, message=FALSE---------------------------------------
+## ---- warning=FALSE, message=FALSE--------------------------------------------
 library("xgboost")
 param <- list(objective = "binary:logistic", max_depth = 2)
 xgb_model <- xgboost(sparse_matrix, params = param, label = titanic_data[, "survived"] == "yes", nrounds = 50, verbose = FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lolli<-lollipop(xgb_model,sparse_matrix)
 plot(lolli, threshold=0.02)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 interactions<-interactions(xgb_model, sparse_matrix, option = "interactions")
 head(interactions, 15)
 plot(interactions)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 importance<-importance(xgb_model, sparse_matrix, option = "both")
 head(importance, 15)
 plot(importance, radar=FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(importance)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 data <- titanic_data[27,]
 new_observation <- sparse_matrix[27,]
 wf<-waterfall(xgb_model, new_observation, data, option = "interactions")
